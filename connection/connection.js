725396d14ca1914049aa886886589e220c74492c
OilPainter1 <hjcryns@gmail.com>
2022-12-09 21:03:29 -0600

    final commit

--- connection/connection.js
+++ connection/connection.js
@@ -1,9 +1,15 @@
 require("dotenv").config()
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_name,process.env.DB_username,process.env.DB_password,{
-    host: "localhost",
-    dialect: "mysql"
-})
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else{
+    sequelize = new Sequelize(process.env.DB_name,process.env.DB_username,process.env.DB_password,{
+        host: "localhost",
+        dialect: "mysql"
+    })
+}
 module.exports = sequelize
